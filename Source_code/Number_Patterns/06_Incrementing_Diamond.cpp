aea58cd98ce1ae2a14af79ea0e58fb39166f479b
Om Singh <omsinghleet@gmail.com>
2026-06-12 12:21:30 +0530

    Added Incrementing Diamond Number pattern and output

---
+++ Source_code/Number_Patterns/06_Incrementing_Diamond.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Upper Part
+    for (int i = 3; i <= 7; i++) {
+
+        for (int j = 3; j <= i; j++) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+
+    // Lower Part
+    for (int i = 6; i >= 3; i--) {
+
+        for (int j = 3; j <= i; j++) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
